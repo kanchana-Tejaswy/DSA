8dfb28041cc84ce1fc0bf86daf350a6749586861
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2026-03-08 21:52:23 +0530

    writen cdoe for array

---
+++ array/array.java
@@ -0,0 +1,15 @@
+public class array {
+    public static void main(String[] args) {
+
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array elements
+        System.out.println("Array elements are:");
+
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
